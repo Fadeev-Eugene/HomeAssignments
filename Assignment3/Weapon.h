dd0e12da6cb9275caa770bebf37fdc606c17ac16
Fadeev-Eugene <st128225@student.spbu.ru>
2024-10-24 18:35:17 +0300

    add weapon class

---
+++ Assignment3/Weapon.h
@@ -0,0 +1,26 @@
+//Fadeev Eugene st128225@student.spbu.ru
+//This header file contains implementation of weapon class for assignment 3.
+
+
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#include <string>
+
+class Weapon {
+protected:
+    std::string type;
+    int damage;
+    
+public:
+    Weapon(const std::string& type, int damage) : type(type), damage(damage) {}
+
+    const std::string& getType() const { return type; }
+    const int& getDamage() const { return damage; }
+
+    void setType(const std::string& type) { this->type = type; }
+    void setDamage(int damage) { this->damage = damage; }
+};
+
+
+#endif
