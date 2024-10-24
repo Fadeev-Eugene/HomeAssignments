e6acea7ca583e51d60d57590cd958457abe6efab
Fadeev-Eugene <st128225@student.spbu.ru>
2024-10-24 18:29:36 +0300

    add dinobot class

---
+++ Assignment3/Dinobot.cpp
@@ -0,0 +1,27 @@
+//Fadeev Eugene st128225@student.spbu.ru
+//This code contains implementation of dinobot class for assignment 3.
+
+
+#include <string>
+#include "Transformer.h"
+
+
+class Dinobot : public Transformer {
+private:
+    std::string rank;
+    std::string role;
+
+public:
+    Dinobot(const std::string& name, unsigned health, unsigned level, unsigned strength, unsigned perception, const Weapon& weapon, const std::string& rank, const std::string& role)
+        : Transformer(name, health, level, strength, perception, weapon), rank(rank), role(role){
+    }
+
+    const std::string& getRank() const { return rank; }
+    const std::string& getRole() const { return role; }
+    
+    void setRank(const std::string& rank) { this->rank = rank; }
+    void setRole(const std::string& role) { this->role = role; }
+
+    bool defend() { return (true); }
+    bool heal() { return (true); }
+};
