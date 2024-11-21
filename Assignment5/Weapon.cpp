f92fa554044e22c2616ebd4d687d1976e44ee3b8
Fadeev-Eugene <st128225@student.spbu.ru>
2024-11-22 01:47:05 +0300

    add a weapon class

---
+++ Assignment5/Weapon.cpp
@@ -0,0 +1,46 @@
+//Fadeev Eugene st128225@student.spbu.ru
+//This code contains implementation of weapon class for assignment 5.
+
+
+#include <string>
+#include <iostream>
+#include "Weapon.h"
+
+Weapon::Weapon(): type("Standart gun"), damage(5) {}
+
+Weapon::Weapon(const std::string& type, int damage) : type(type), damage(damage) {}
+
+const std::string& Weapon::getType() const
+{
+    return type;
+}
+const int& Weapon::getDamage() const
+{
+    return damage;
+}
+
+void Weapon::setType(const std::string& type)
+{
+    this->type = type;
+}
+void Weapon::setDamage(int damage)
+{
+    this->damage = damage;
+}
+
+std::ostream& operator<<(std::ostream& os, const Weapon& t)
+{
+    os << "Type: " << t.getType() << "\n"
+       << "Damage: " << t.getDamage();
+    return os;
+}
+
+bool Weapon::operator>(const Weapon& other) const
+{
+    return damage > other.damage;
+}
+
+bool Weapon::operator<(const Weapon& other) const
+{
+    return damage < other.damage;
+}
---
+++ Assignment5/Weapon.h
@@ -0,0 +1,36 @@
+//Fadeev Eugene st128225@student.spbu.ru
+//This header file contains implementation of weapon class for assignment 5.
+
+
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#include <string>
+#include <iostream>
+
+class Weapon
+{
+protected:
+    std::string type;
+    int damage;
+
+public:
+    Weapon();
+
+    Weapon(const std::string& name, int damage);
+
+    const std::string& getType() const;
+    const int& getDamage() const;
+
+    void setType(const std::string& name);
+    void setDamage(int speed);
+
+    friend std::ostream& operator<<(std::ostream& os, const Weapon& t);
+
+    bool operator>(const Weapon& other) const;
+
+    bool operator<(const Weapon& other) const;
+};
+
+
+#endif
